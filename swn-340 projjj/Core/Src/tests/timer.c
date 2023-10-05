03920da621c6bcf46553b5788fb51cc2e1422068
MarMariMarisa <96095835+MarMariMarisa@users.noreply.github.com>
2023-10-04 23:32:14 -0400

    homework two!

--- swn-340 projjj/Core/Src/tests/timer.c
+++ swn-340 projjj/Core/Src/tests/timer.c
@@ -11,7 +11,7 @@ void init_systick(){
 	//Create a SYST and initalize it with an RVR of 100,000
 	//turn it on
 	SYST syst;
-	syst->RVR = 100000;
-	syst->CSR = syst->CSR | (1 << 0);
+	syst->RVR = 99999;//(99999+1) / 8000000 sec timer
+	syst->CSR = syst->CSR | (1 << 0); //set bit 0 to turn on
 
 }
