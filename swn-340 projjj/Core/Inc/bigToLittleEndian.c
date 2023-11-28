d2998dde6fabb5db1a474333ca73fdf6e0f9eb9e
MarMariMarisa <96095835+MarMariMarisa@users.noreply.github.com>
2023-11-27 21:32:02 -0500

    hw assignments:

---
+++ swn-340 projjj/Core/Inc/bigToLittleEndian.c
@@ -0,0 +1,17 @@
+/*
+ * bigToLittleEndian.c
+ *
+ *  Created on: Nov 27, 2023
+ *      Author: Marisa
+ */
+
+#ifndef CORE_INC_BIGTOLITTLEENDIAN_C_
+#define CORE_INC_BIGTOLITTLEENDIAN_C_
+
+#include "stdint.h"
+
+
+#endif /* CORE_INC_BIGTOLITTLEENDIAN_C_ */
+
+uint16_t convert_to_uint16 (uint8_t* p_value);
+uint32_t convert_to_uint32 (uint8_t* p_value);
